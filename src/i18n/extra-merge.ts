612d40ba3e53ff741e968d3395f294492ec67710
AlexManel <99820651+AlexManel@users.noreply.github.com>
2026-08-24 18:10:27 +0300

    Merge leftover UI strings into all locales

--- src/i18n/extra-merge.ts
+++ src/i18n/extra-merge.ts
@@ -10,22 +10,68 @@ import {
   resourceKeysFr,
 } from "./resource-keys";
 import { catalogKeys } from "./catalog-keys";
+import {
+  leftoverEn,
+  leftoverEl,
+  leftoverDe,
+  leftoverRu,
+  leftoverTr,
+  leftoverFr,
+} from "./leftover-keys";
 
 type Dict = Record<string, string>;
 
 export const extra: Record<Locale, Dict> = {
-  en: { ...portalKeysEn, ...resourceKeysEn, ...catalogKeys.en, ...base.en },
+  en: { ...portalKeysEn, ...resourceKeysEn, ...catalogKeys.en, ...leftoverEn, ...base.en },
   el: {
     ...portalKeysEn,
     ...resourceKeysEn,
     ...catalogKeys.en,
+    ...leftoverEn,
     ...portalKeysEl,
     ...resourceKeysEl,
     ...catalogKeys.el,
+    ...leftoverEl,
     ...base.el,
   },
-  de: { ...portalKeysEn, ...resourceKeysEn, ...catalogKeys.en, ...resourceKeysDe, ...catalogKeys.de, ...base.de },
-  ru: { ...portalKeysEn, ...resourceKeysEn, ...catalogKeys.en, ...resourceKeysRu, ...catalogKeys.ru, ...base.ru },
-  tr: { ...portalKeysEn, ...resourceKeysEn, ...catalogKeys.en, ...resourceKeysTr, ...catalogKeys.tr, ...base.tr },
-  fr: { ...portalKeysEn, ...resourceKeysEn, ...catalogKeys.en, ...resourceKeysFr, ...catalogKeys.fr, ...base.fr },
+  de: {
+    ...portalKeysEn,
+    ...resourceKeysEn,
+    ...catalogKeys.en,
+    ...leftoverEn,
+    ...resourceKeysDe,
+    ...catalogKeys.de,
+    ...leftoverDe,
+    ...base.de,
+  },
+  ru: {
+    ...portalKeysEn,
+    ...resourceKeysEn,
+    ...catalogKeys.en,
+    ...leftoverEn,
+    ...resourceKeysRu,
+    ...catalogKeys.ru,
+    ...leftoverRu,
+    ...base.ru,
+  },
+  tr: {
+    ...portalKeysEn,
+    ...resourceKeysEn,
+    ...catalogKeys.en,
+    ...leftoverEn,
+    ...resourceKeysTr,
+    ...catalogKeys.tr,
+    ...leftoverTr,
+    ...base.tr,
+  },
+  fr: {
+    ...portalKeysEn,
+    ...resourceKeysEn,
+    ...catalogKeys.en,
+    ...leftoverEn,
+    ...resourceKeysFr,
+    ...catalogKeys.fr,
+    ...leftoverFr,
+    ...base.fr,
+  },
 };
